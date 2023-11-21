0955daa5180051e47c2fcec802a998a6ddd3512a
MLucasDelgado <lucasdelgado1011@gmail.com>
2023-11-21 12:05:57 -0300

    next

--- src/app/components/TaskCard.jsx
+++ src/app/components/TaskCard.jsx
@@ -5,10 +5,11 @@ const TaskCard = ({ task }) => {
     const router = useRouter()
 
     return (
-        <div className='bg-slate-900 p-3 rounded hover:bg-slate-800 hover:cursor-pointer hover:animate-pulse'
-        onClick={() => {
-            router.push('/task/edit/' + task.id)
-        }}>
+        <div
+            className='bg-slate-900 p-3 rounded hover:bg-slate-800 hover:cursor-pointer hover:animate-pulse'
+            onClick={() => {
+                router.push('/task/edit/' + task.id)
+            }}>
             <h3 className='mb-2 font-bold'>{task.title}</h3>
             <p>{task.description}</p>
             <p>{new Date(task.createdAt).toLocaleDateString()}</p>
